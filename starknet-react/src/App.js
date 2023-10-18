9893688da75e7244955cdc28fe91c6342d28a139
Darlington02 <darlingtonnnam@gmail.com>
2023-10-18 01:27:37 +0100

    updated boilerplate

--- starknet-react/src/App.js
+++ starknet-react/src/App.js
@@ -1,11 +1,8 @@
-import StarknetProvider from './components/starknet-provider'
 import Home from './components/home'
 
 function App() {
   return (
-    <StarknetProvider>
       <Home />
-    </StarknetProvider>
   );
 }
 
