11c7e3772eb9afd5d975abcdfb1ff505754ffa80
Darlington02 <darlingtonnnam@gmail.com>
2023-10-18 01:25:19 +0100

    updated boilerplate

---
+++ starknet-react/src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useAccount, useConnect, useDisconnect } from "@starknet-react/core"
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
---
+++ starknet-react/src/components/starknet-provider.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { InjectedConnector } from "starknetkit/injected";
+import { ArgentMobileConnector } from "starknetkit/argentMobile";
+import { WebWalletConnector } from "starknetkit/webwallet";
+import { goerli, mainnet } from "@starknet-react/chains";
+import { StarknetConfig, publicProvider } from "@starknet-react/core";
+
+export default function StarknetProvider({children}) {
+    
+}
